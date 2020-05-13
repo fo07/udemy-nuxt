05b9f455ab8ea6719e91410d1ec99b09c23b33b8
fo <fo.shuukatsu@gmail.com>
2020-05-13 17:38:17 +0900

    add: エラーページ設定

---
+++ nuxt-view/layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Error{{ error.statusCode }}</h1>
+    <!-- <nuxt /> -->
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['error']
+}
+</script>
