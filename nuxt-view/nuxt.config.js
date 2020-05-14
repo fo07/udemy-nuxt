5ea38b63ef5d141312e4ff8d658381148478ebc4
fo <fo.shuukatsu@gmail.com>
2020-05-14 09:49:53 +0900

    add: CSSの変更

--- nuxt-view/nuxt.config.js
+++ nuxt-view/nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap' }
     ]
   },
   /*
@@ -46,7 +47,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
