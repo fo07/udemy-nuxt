a7cf585d5050e5db08853c6499060b9f72b11939
fo <fo.shuukatsu@gmail.com>
2020-05-15 10:21:13 +0900

    update: 日付ソート表示実装

--- nuxt-todo/pages/todos.vue
+++ nuxt-todo/pages/todos.vue
@@ -51,7 +51,8 @@ export default {
   },
   computed: {
     todos() {
-      return this.$store.state.todos.todos;
+      // return this.$store.state.todos.todos;
+      return this.$store.getters['todos/orderdTodos'];
     }
   },
   filters: {
